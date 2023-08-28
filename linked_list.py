b264a947846ad2d58c0205fa810bd2aeddc8aae7
Dhanush <coder.dhanush@gmail.com>
2023-08-28 17:45:50 +0530

    Linked list implementation

---
+++ linked_list.py
@@ -0,0 +1,162 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def prepend(self, value) -> bool:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def append(self, value) -> bool:
+        new_node = Node(value)
+        if self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
+
+    def pop_first(self) -> None:
+        if self.head is None:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def pop(self):
+        if not self.head:
+            return None
+        if self.head == self.tail:
+            removed = self.head
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            while temp.next != self.tail:
+                temp = temp.next
+            removed = temp.next
+            temp.next = None
+            self.tail = temp
+        self.length -= 1
+        return removed
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return True
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+    def find_middle_node(self):
+        slow_ptr = self.head
+        fast_ptr = self.head
+        while fast_ptr and fast_ptr.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+        return slow_ptr
+
+    def has_loop(self):
+        slow_ptr = self.head
+        fast_ptr = self.head
+        while fast_ptr and fast_ptr.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+            if slow_ptr == fast_ptr:
+                return True
+        return False
+
+    def print_list(self) -> None:
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+def find_kth_from_end(self, k):
+    slow_ptr = self.head
+    fast_ptr = self.head
+    for _ in range(k):
+        if fast_ptr is None:
+            return None
+        fast_ptr = fast_ptr.next
+    while fast_ptr:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next
+    return slow_ptr
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.reverse()
+my_linked_list.reverse()
+my_linked_list.print_list()
