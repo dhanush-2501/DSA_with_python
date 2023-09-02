5d3624da8b12499ac1ae0b35c06fbdb36c977130
DHANUSH N <coder.dhanush@gmail.com>
2023-09-02 06:36:31 +0530

    singly Linked list completed
    * added remove_duplicate and bin_to_dec
    
    * reverse betweeb function added

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -138,7 +138,51 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
+    def remove_duplicates(self):
+        seen = set()
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.value in seen:
+                prev.next = curr.next
+                self.length -= 1
+            else:
+                seen.add(curr.value)
+                prev = curr
+            curr = curr.next
 
+    def binary_to_decimal(self):
+        temp = self.head
+        count = -1
+        while temp:
+            count += 1
+            temp = temp.next
+        temp = self.head
+        sum = 0
+        while temp:
+            sum += 2 ** count * temp.value
+            count -= 1
+            temp = temp.next
+        return sum
+    
+    def reverse_between(self, m, n):
+        if self.length <= 1:
+            return
+        dummy = Node(0)
+        dummy.next = self.head
+        prev = dummy
+        for i in range(m):
+            prev = prev.next
+        current = prev.next
+        for i in range(n - m):
+            temp = current.next
+            current.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+        self.head = dummy.next
+
+        
 
 def find_kth_from_end(self, k):
     slow_ptr = self.head
@@ -151,12 +195,3 @@ def find_kth_from_end(self, k):
         slow_ptr = slow_ptr.next
         fast_ptr = fast_ptr.next
     return slow_ptr
-
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
-my_linked_list.reverse()
-my_linked_list.reverse()
-my_linked_list.print_list()
